c0760cf62db3f03d9bb6218591b44f19782b5e03
eketeUg <emmanuelugochukwuekete@gmail.com>
2025-08-08 12:22:11 +0100

    pkg fix

--- install-backend-linux.sh
+++ install-backend-linux.sh
@@ -1,10 +1,8 @@
- #!/bin/bash
+#!/bin/bash
 set -e
-
 
 VERSION="v1.0.0"
 BINARY_URL="https://github.com/skillDeCoder/Idle-finance-v2-backend/releases/download/v1.0.0/backend-linux"
-
 ENV_FILE_ID="1H8uAgVARe4GkMHNU2usNc73u39GjHndu"
 
 echo "
@@ -20,18 +18,11 @@ echo "
 echo "Downloading backend..."
 curl -L "${BINARY_URL}" -o backend
 
-
 echo "Making backend executable..."
 chmod +x backend
 
-
 echo "Downloading .env from Google Drive..."
 curl -L -o .env "https://drive.google.com/uc?export=download&id=${ENV_FILE_ID}"
-
-
-echo "Loading .env variables..."
-export $(grep -v '^#' .env | xargs)
-
 
 
 echo "Running backend..."
